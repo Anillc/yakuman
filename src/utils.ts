ca288d320d795d929f070239f7171539bc7f604c
Anillc <i@anillc.cn>
2023-05-06 14:11:56 +0800

    group with number

--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,26 @@
-import type { Tile, TileType } from '.'
+import type { Tile } from '.'
+
+export interface Counts {
+  man: number[],
+  so: number[],
+  pin: number[],
+  kaze: number[],
+  sangen: number[],
+}
+
+export function createEmptyCounts(): Counts {
+  return {
+    man:    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    so:     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    pin:    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    kaze:   [0, 0, 0, 0],
+    sangen: [0, 0, 0],
+  }
+}
 
 export function group(tiles: Tile[]) {
   return tiles.reduce((acc, x) => {
-    acc[x.type][x.num - 1].push(x)
+    acc[x.type][x.num - 1]++
     return acc
-  }, {
-    'man': [...Array(9)].map(_ => []),
-    'pin': [...Array(9)].map(_ => []),
-    'so': [...Array(9)].map(_ => []),
-    'kaze': [...Array(4)].map(_ => []),
-    'sangen': [...Array(3)].map(_ => []),
-  } as Record<TileType, Tile[][]>)
+  }, createEmptyCounts())
 }
