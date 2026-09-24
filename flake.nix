da854d11e647ee02c28c9c005f1ad1cda9c514f5
Anillc <void@anil.lc>
2026-09-24 17:19:05 +0800

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  outputs = inputs@{
+    self, nixpkgs, flake-parts,
+  }: flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    perSystem = { pkgs, ... }: {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [];
+        nativeBuildInputs = with pkgs; [ nodejs yarn-berry ];
+      };
+    };
+  };
+}
